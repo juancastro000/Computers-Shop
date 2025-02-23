6e999f03b0fbb9795b52f0b97bde944f49704878
juancastro000 <cdyutub@gmail.com>
2025-02-23 16:39:24 +0100

    test red getters not implemented on computer

--- src/test/java/dev/juan/computers_shop/ComputerTest.java
+++ src/test/java/dev/juan/computers_shop/ComputerTest.java
@@ -12,4 +12,14 @@ public class ComputerTest {
         assertThat(pc, is(notNullValue()));
 
     }
+
+    @Test
+    void testGetComputer() {
+        Computer pc = new Computer("Dell", 16, "Intel i7", "Windows", 1500);
+        assertThat(pc.getBrand(), is("dell") );
+        assertThat(pc.getMemory(),is(16));
+        assertThat(pc.getCpu(),is("Intel i7"));
+        assertThat(pc.getOperatingSystem(),is("Windows"));
+        assertThat(pc.getPrice(),is(1500));
+    }
 }
