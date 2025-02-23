dd682952868f1548657889e2e9453e253604a1b7
juancastro000 <cdyutub@gmail.com>
2025-02-23 19:17:37 +0100

    delete app

--- src/main/java/dev/juan/computers_shop/App.java
+++
@@ -1,9 +0,0 @@
-package dev.juan.computers_shop;
-public final class App {
-    private App() {}
-
-    public static void main(String[] args) {
-        System.out.println("My Java Template");
-
-    }
-}
