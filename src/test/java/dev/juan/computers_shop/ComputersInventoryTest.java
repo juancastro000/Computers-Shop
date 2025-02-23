a0efeef2683995197b6610ccc3d64cdbce888d0b
juancastro000 <cdyutub@gmail.com>
2025-02-23 17:28:41 +0100

    test red delete computer from inventory is not implemented

--- src/test/java/dev/juan/computers_shop/ComputersInventoryTest.java
+++ src/test/java/dev/juan/computers_shop/ComputersInventoryTest.java
@@ -1,8 +1,7 @@
 package dev.juan.computers_shop;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -20,6 +19,19 @@ public class ComputersInventoryTest {
         
         assertThat(list, hasSize(1));
         assertThat(list, hasItem(pc));
+    }
+
+    @Test
+    void testDeleteComputerFromInventory(){
+        ComputersInventory inventory = new ComputersInventoryImpl();
+        Computer pc = new Computer("HP", 8, "AMD", "Linux", 1000);
+
+        inventory.addComputer(pc);
+        inventory.deleteComputer(pc);
+        List<Computer> list = inventory.listed();
+
+        assertThat(list, hasSize(0));
+        assertThat(list, not(hasItem(pc)));
     }
 
 }
